c4097a70ccf37501ce1c6bad79aea5ea863371c1
Eric Stine <ericstine@Erics-MacBook-Pro.local>
2023-04-03 01:54:04 -0400

    Tests 1-6 working, will look to refactor.

--- wzip/src/wzip.c
+++ wzip/src/wzip.c
@@ -1,54 +1,42 @@
-// wzip source code here.
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Parse command-line arguments
   if (argc < 2) {
-    fprintf(stderr, "wzip: file1 [file2 ...]\n");
-    return 1;
+    fprintf(stdout, "wzip: file1 [file2 ...]\n");
+    exit(1);
   }
 
-  // Initialize variables
   int counter = 0;
-  int curr_char, prev_char;
+  char prev_char = 0;
+  char current_char;
 
-  // Loop through input files
-  for (int i = 1; i < argc; ++i) {
-    char *input_file = argv[i];
-
-    // printf("Processing file: %s\n", input_file);
-
-    // Open input file
-    FILE *curr_file = fopen(input_file, "r");
-
-    // Initialize the prev_char with the first character from the input file.
-    prev_char = fgetc(curr_file);
-    counter = 1;
-
-    // Read input file and perform run-length encoding
-    while ((curr_char = fgetc(curr_file)) != EOF) {
-      if (curr_char == prev_char) {
-        // Characters are the same, increment the counter.
-        counter++;
-      } else {
-        // Compare characters and update counter or write to stdout
-        fwrite(&counter, sizeof(int), 1, stdout);
-        fwrite(&prev_char, sizeof(char), 1, stdout);
-
-        // Reset the counter and update the previous character.
-        counter = 1;
-        prev_char = curr_char;
-      }
+  for (int i = 1; i < argc; i++) {
+    FILE *file = fopen(argv[i], "r");
+    if (file == NULL) {
+      fprintf(stdout, "Error opening file: %s\n", argv[i]);
+      exit(1);
     }
 
-    // Handle the last run-length
-    fwrite(&counter, sizeof(int), 1, stdout);
-    fwrite(&prev_char, sizeof(char), 1, stdout);
-
-    // Close input file
-    fclose(curr_file);
+    while (fread(&current_char, sizeof(char), 1, file) == 1) {
+      if (current_char != prev_char) {
+        if (counter > 0) {
+          fwrite(&counter, sizeof(int), 1, stdout);
+          fwrite(&prev_char, sizeof(char), 1, stdout);
+        }
+        counter = 1;
+        prev_char = current_char;
+      } else {
+        counter++;
+      }
+    }
+    fclose(file);
   }
 
-  // Exit with return code 0
+  if (counter > 0) {
+    fwrite(&counter, sizeof(int), 1, stdout);
+    fwrite(&prev_char, sizeof(char), 1, stdout);
+  }
+
   return 0;
 }
